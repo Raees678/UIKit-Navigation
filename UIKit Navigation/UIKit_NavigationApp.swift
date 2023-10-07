a5ebeb8eac8137c0d19acbade15ff0a10105eeb7
Raees Rajwani <raeesrajwani95@gmail.com>
2023-10-07 15:21:50 +0530

    Initial Commit

---
+++ UIKit Navigation/UIKit_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKit_NavigationApp.swift
+//  UIKit Navigation
+//
+//  Created by Raees Rajwani on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct UIKit_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
